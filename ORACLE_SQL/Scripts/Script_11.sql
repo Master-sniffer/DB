da7fef82203c06afad04469c05a7c26659caaeac
Master_Sniffer <61323659+Master-sniffer@users.noreply.github.com>
2021-04-01 13:13:44 +0300

    Create Script_11.sql

---
+++ ORACLE_SQL/Scripts/Script_11.sql
@@ -0,0 +1,46 @@
+An entry in a table, consisting of values for each appropriate column. - row 
+ 
+The set of mandatory columns within a table that is used to enforce uniqueness of rows, and that is normally the most frequent means by which rows are accessed. - primary key 
+ 
+An arrangement of data in rows and columns. - Table 
+ 
+A column or set of columns that refers to a primary key in the same table or another table. -  foreign key 
+ 
+Collections of objects or relations, set of operators to act on those relations, and data integrity for accuracy and consistency -  relational dB 
+ 
+Intersection of a row and column  -  field 
+ 
+Used to modify the table data by entering, changing, or removing rows - data manipulation language 
+ 
+Creates, changes, and removes data structures from the database - data definition language 
+ 
+Used to manage the changes made by DML statements - transaction control language  
+ 
+Used to give or remove access rights to the database and the structures within it -  data control language 
+ 
+ 
+ 
+1. The Global Fast Foods database consists of how many tables? 9 tables  
+  
+  
+2. How is the F_SHIFTS table related to the F_STAFFS table? 
+There is no relation 
+ 
+3. What are the names of the columns in the F_CUSTOMERS table? 
+ID, FIRST_NAME, LAST_NAME, ADDRESS, CITY, STATE, ZIP, PHONE NUMBER 
+4. How many rows of data have been entered in the F_PROMOTIONAL_MENUS table? 
+2 
+5. In the F_FOOD_ITEMS table, column _________ is a foreign-key column. What table and column is this key referencing? 
+Promo_code and Regular_code are foreign-keys. F_PROMOTIONAL_MENUS and F_REGULAR_MENUS respectively. 
+6. List the primary key to foreign key relationships required to go from the F_SHIFTS table to the F_REGULAR_MENUS table. 
+Code – regular code 
+Code- code  
+Id- - ID 
+Stuff id  
+Order number – order number 
+Food item number – food item number 
+7. Which table(s) contains null values? 
+F_STAFF has null values 
+ 
+ 
+ 
